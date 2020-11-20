fcd349514ae58ec86adbfe58418adcfb8d89eef3
Heath Brames <hbrames@umass.edu>
2020-11-20 13:37:49 -0500

    initial convert to react app

---
+++ src/components/LandingPage/Home.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './Home.css';
+
+
+
+
+
+const Home = (props)=> {
+    return (
+        <div className="AppTwo">
+        <nav className="navbar navbar-expand-lg navbar-dark sticky-top" id="navigation-bar">
+
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#"><img src="images/WebsiteLogoPNG3.png" /></a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" 
+                data-target="#navbarResponsive">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="index.html">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="https://medium.com/@gatorsunis">News</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="football.html">Football</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="https://www.behance.net/gallery/96730413/Florida-Gators-Concept-Uniforms-GatorsUnis">Concepts</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="index.html">About</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+
+        </div>
+
+    )
+
+};
+
+export default Home;
