b812d1765af6f32a11d23ee65a243b6a5b1782e1
Heath Brames <hbrames@umass.edu>
2020-11-20 13:47:06 -0500

    changed v1

--- src/components/LandingPage/Home.js
+++ src/components/LandingPage/Home.js
@@ -7,7 +7,7 @@ import './Home.css';
 
 const Home = (props)=> {
     return (
-        <div className="AppTwo0">
+        <div className="AppTwo">
         <nav className="navbar navbar-expand-lg navbar-dark sticky-top" id="navigation-bar">
 
             <div className="container-fluid">
